bc944f1de858d37b77e286a79cad1199487c0462
hazkerindii228 <59193313+PavelGem-13g@users.noreply.github.com>
2023-05-18 00:25:20 +0300

    add web interface

--- project/Work/Library/plots.py
+++ project/Work/Library/plots.py
@@ -22,40 +22,37 @@ def generate_html_image(fig, name):
 
 
 def generate_plots(df):
-    result = ''
+    result = []
     
     fig = px.bar(df, x='CAGR', y='Ranked2020', color='Ranked2021', title='Relation between CAGR and Ranked2020', width=1000, height=500, template='plotly_white')
-    result += generate_html_image(fig, "r2021.svg")
+    result.append(generate_html_image(fig, "r2021.svg"))
 
     fig = px.histogram(df, x='Sector', color='Sector', title="The most common sectors", width=1000, height=500, template='plotly_white').update_xaxes(categoryorder="total descending")
-    result += generate_html_image(fig, "sector.svg")
+    result.append(generate_html_image(fig, "sector.svg"))
     
     fig = px.scatter(df, x='Revenue2017', y='Revenue2020', color='Sector', size="CAGR",animation_frame=df['Sector'], width=1000, height=500, template='plotly_white')
-    result += generate_html_image(fig, "CAGRRevenue2020Sector.svg")
+    result.append(generate_html_image(fig, "CAGRRevenue2020Sector.svg"))
 
     fig = px.histogram(df, x='Country',color='Country', title= 'The most common countries', width=1000, height=500,template='plotly_white').update_xaxes(categoryorder="total descending")
-    result += generate_html_image(fig, "countries.svg")
+    result.append(generate_html_image(fig, "countries.svg"))
     
     df = df.sort_values(by="FoundingYear")
     fig=px.choropleth(data_frame=df,locations=df['Country'],locationmode='country names',color=df['Revenue2020'],animation_frame=df['FoundingYear'],animation_group=df['CAGR'], width=1000, height=500, template='plotly_white')
     fig.update_layout(dict1={'title':'Revenue distribution relative to countries for 2020 on the map'})
-    result += generate_html_image(fig, "Revenue2020 FoundingYear.svg")
+    result.append(generate_html_image(fig, "Revenue2020 FoundingYear.svg"))
     
     fig = px.line(df.sort_values(by="FoundingYear"), x='FoundingYear', y='CAGR', title='Relation between CAGR and Founding Year', width=1000, height=500, template='plotly_white')
-    result += generate_html_image(fig, "FoundingCagr.svg")
+    result.append(generate_html_image(fig, "FoundingCagr.svg"))
     
     fig = px.box(df, x='Sector', color='Sector', y='Employees2017', title='Distribution and analysis of the number of employees by sector in 2017', width=1000, height=500, template='plotly_white')
-    result += generate_html_image(fig, "Sector Employees2017.svg")
+    result.append(generate_html_image(fig, "Sector Employees2017.svg"))
     
     fig = px.imshow(df[["Name","Ranked2021","Ranked2020","Country","Sector","CAGR","Revenue2020","Revenue2017","Employees2020","Employees2017","FoundingYear"]].corr(),title="Correlations", width=1000, height=500, template='plotly_white')
-    result += generate_html_image(fig, "Correlation.svg")
+    result.append(generate_html_image(fig, "Correlation.svg"))
 
     return result
-
     
 
 if __name__ == "__main__":
     df = data_loader.get_database()
-    html = '<script src="https://cdn.plot.ly/plotly-latest.min.js"></script>'
-    html += generate_plots(df)
-    
+    page = generate_plots(df)
--- project/Work/Scripts/main.py
+++ project/Work/Scripts/main.py
@@ -1,6 +1,5 @@
 # -*- coding: utf-8 -*-
 
-import sys
 import os
 
 os.chdir("../Library")
@@ -8,9 +7,32 @@ import data_loader
 import plots
 import sorter
 
-import pandas
+os.chdir("../Scripts")
 
-if __name__ == "__main__":
-    df = data_loader.get_database()
-    sorter.makeOutputs(df)
-    plots.generate_plots(df)
+# if __name__ == "__main__":
+#     df = data_loader.get_database()
+#     sorter.makeOutputs(df)
+#     plots.generate_plots(df)
+
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def upload_dataset():
+    return render_template("index.html")
+
+@app.route('/success', methods=['POST'])
+def success():
+    if request.method == 'POST':
+        f = request.files['file']
+        f.save(f.filename)
+        os.replace(f.filename, "../Data/"+f.filename)
+        
+        df = data_loader.get_database()
+        plt = plots.generate_plots(df)
+        return render_template("work.html", name=f.filename, plots=plt)
+
+if __name__=="__main__":
+    app.run()
